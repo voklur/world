2b364f7adca0a019734e8aa7fa111cb3765ef21a
voklur <voklur@gmail.com>
2022-11-28 11:53:46 -0500

    Update mysql.sql

--- mysql.sql
+++ mysql.sql
@@ -1,5 +1,5 @@
 create database if not exists `real_world`;
 use real_world;
-create user 'realworld' identified by 'here-secret';
+create user 'realworld'@'localhost' identified by 'here-secret';
 grant all privileges on real_world.* to 'realworld'@'localhost';
 
